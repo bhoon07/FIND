661cbdbb1a8d2b8f426ede03733faa029b8f796a
Blaine Hoon <bhoon07@gmail.com>
2020-10-16 11:30:08 -0500

    updates

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./src/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/index.html"));
+  app.use(express.static("index.html"));
 }
 app.use(routes);
 
