3d30a375e3ec5b2be7a4ba164a48f00db90304e7
Blaine Hoon <bhoon07@gmail.com>
2020-10-18 23:45:15 -0500

    updates

---
+++ client/build/precache-manifest.fee5318fc976ab84e2e9d14f140416c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e717777c1c5ee06efcd1cd39926174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2517e7082d6fcbc370f3",
+    "url": "/static/css/main.641fe642.chunk.css"
+  },
+  {
+    "revision": "a772f5646685c85634d6",
+    "url": "/static/js/2.bdc43e03.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.bdc43e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2517e7082d6fcbc370f3",
+    "url": "/static/js/main.37a634f4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
